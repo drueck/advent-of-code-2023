3f2b17d277d31c825ed1865ca22cde800d6947ca
David Rueck <drueck@gmail.com>
2023-12-07 21:22:36 -0800

    Day 5, Part 2

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -2,15 +2,24 @@
 // https://adventofcode.com/2023/day/5
 // Usage: `cargo run <input-file>
 
+// credit for the part two algorithm goes to reddit user zuleyorker.
+
 use day_05::map::Map;
 use regex::Regex;
+use std::collections::HashSet;
 use std::env;
 use std::fs;
+use std::ops::RangeInclusive;
 
 fn main() {
     let input_filename = env::args().nth(1).expect("please supply an input filename");
     let input = fs::read_to_string(input_filename).expect("failed to read input");
 
+    part_one(&input);
+    part_two(&input);
+}
+
+fn part_one(input: &str) {
     let mut sections = input.trim().split("\n\n");
 
     let seeds_re = Regex::new(r"(\d+)").unwrap();
@@ -28,5 +37,56 @@ fn main() {
 
     let lowest_location = locations.iter().min().unwrap();
 
-    println!("The lowest location was {}", lowest_location);
+    println!(
+        "The lowest location with part one rules is: {}",
+        lowest_location
+    );
+}
+
+fn part_two(input: &str) {
+    let mut sections = input.trim().split("\n\n");
+
+    let seeds_re = Regex::new(r"(\d+)").unwrap();
+
+    let seed_ranges: Vec<RangeInclusive<usize>> = seeds_re
+        .find_iter(sections.next().unwrap())
+        .map(|m| m.as_str().parse().unwrap())
+        .collect::<Vec<usize>>()[..]
+        .chunks(2)
+        .map(|pair| pair[0]..=(pair[0] + pair[1] - 1))
+        .collect();
+
+    let maps: Vec<Map> = sections.map(|s| s.parse().unwrap()).collect();
+
+    // try to find a smaller number of seeds to check by working backward through the maps
+    let mut seeds_to_check = maps
+        .iter()
+        .rev()
+        // start with the endpoints of the whole usize range
+        .fold(HashSet::from([0, usize::MAX]), |set, map| {
+            // for each map working backward
+            // translate all current values by reversing the mapping
+            let mut updated: HashSet<_> = set.iter().map(|&value| map.get_reverse(value)).collect();
+            // then add the endpoints from the map's source range
+            updated.extend(map.source_range_endpoints());
+            updated
+        });
+
+    // limit the endpoints to check to values in the known seed ranges
+    seeds_to_check.retain(|value| seed_ranges.iter().any(|range| range.contains(value)));
+
+    // add the endpoints of the seed ranges themselves
+    seeds_to_check.extend(seed_ranges.iter().flat_map(|r| vec![r.start(), r.end()]));
+
+    // now that we have a limited number of seeds to check, translate to locations and get the min
+    let locations = maps.iter().fold(seeds_to_check, |results, map| {
+        results.iter().map(|&source| map.get(source)).collect()
+    });
+
+    let lowest_location = locations.iter().min().unwrap();
+
+    println!(
+        "The lowest location with part two rules is: {}",
+        lowest_location
+    );
 }
--- day-05/src/map.rs
+++ day-05/src/map.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::collections::HashSet;
 use std::ops::RangeInclusive;
 use std::str::FromStr;
 
@@ -21,6 +22,39 @@ impl Map {
             }
         }
         source
+    }
+
+    pub fn get_reverse(&self, destination: usize) -> usize {
+        for range_i in 0..self.destination_ranges.len() {
+            let source_range = &self.source_ranges[range_i];
+            let destination_range = &self.destination_ranges[range_i];
+
+            if destination_range.contains(&destination) {
+                return source_range.start() + (destination - destination_range.start());
+            }
+        }
+        destination
+    }
+
+    /// return a set containing the start and end of each range,
+    /// as well as the values lower and higher than each range
+    /// which form the endpoints of the bounds outside the specified ranges
+    /// we exclude 0 and usize::MAX because those are assumed
+    pub fn source_range_endpoints(&self) -> HashSet<usize> {
+        self.source_ranges
+            .iter()
+            .fold(HashSet::new(), |mut endpoints, range| {
+                let start = *range.start();
+                let end = *range.end();
+
+                if start > 1 {
+                    endpoints.insert(start - 1);
+                }
+                endpoints.insert(start);
+                endpoints.insert(end);
+                endpoints.insert(end + 1);
+                endpoints
+            })
     }
 }
 
@@ -64,6 +98,7 @@ impl FromStr for Map {
 #[cfg(test)]
 pub mod tests {
     use crate::map::Map;
+    use std::collections::HashSet;
 
     #[test]
     fn test_parse() {
@@ -74,5 +109,40 @@ pub mod tests {
         assert_eq!(map.to, "soil");
         assert_eq!(map.source_ranges, vec![98..=99, 50..=97]);
         assert_eq!(map.destination_ranges, vec![50..=51, 52..=99]);
+    }
+
+    #[test]
+    fn test_get() {
+        let text = "seed-to-soil map:\n50 98 2\n52 50 48";
+        let map: Map = text.parse().unwrap();
+
+        assert_eq!(map.get(98), 50);
+        assert_eq!(map.get(99), 51);
+        assert_eq!(map.get(50), 52);
+        assert_eq!(map.get(55), 57);
+        assert_eq!(map.get(97), 99);
+    }
+
+    #[test]
+    fn test_get_reverse() {
+        let text = "seed-to-soil map:\n50 98 2\n52 50 48";
+        let map: Map = text.parse().unwrap();
+
+        assert_eq!(map.get_reverse(50), 98);
+        assert_eq!(map.get_reverse(51), 99);
+        assert_eq!(map.get_reverse(52), 50);
+        assert_eq!(map.get_reverse(57), 55);
+        assert_eq!(map.get_reverse(99), 97);
+    }
+
+    #[test]
+    fn test_source_range_endpoints() {
+        let text = "seed-to-soil map:\n50 98 2\n52 50 48";
+        let map: Map = text.parse().unwrap();
+
+        assert_eq!(
+            map.source_range_endpoints(),
+            HashSet::from([49, 50, 97, 98, 99, 100])
+        )
     }
 }
