c88e3eea6243cf5a4a32254b2bcacae98f1c1787
David Rueck <drueck@gmail.com>
2023-12-12 15:43:14 -0800

    Day 8, Part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -2,13 +2,11 @@
 // https://adventofcode.com/2023/day/8
 // Usage: `cargo run <input-file>
 
+use primes::{PrimeSet, Sieve};
 use regex::Regex;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs;
-
-const START: &str = "AAA";
-const END: &str = "ZZZ";
 
 fn main() {
     let input_filename = env::args().nth(1).expect("please supply an input filename");
@@ -28,17 +26,26 @@ fn main() {
         })
         .collect();
 
-    let re = Regex::new(r"([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)").unwrap();
+    let re = Regex::new(r"(.{3}) = \((.{3}), (.{3})\)").unwrap();
 
     let map: HashMap<&str, [&str; 2]> = parts
         .next()
         .unwrap()
+        .trim()
         .split('\n')
         .map(|line| {
             let (_, map_parts): (&str, [&str; 3]) = re.captures(&line).unwrap().extract();
             (map_parts[0], [map_parts[1], map_parts[2]])
         })
         .collect();
+
+    part_one(&map, &directions);
+    part_two(&map, &directions);
+}
+
+fn part_one(map: &HashMap<&str, [&str; 2]>, directions: &Vec<usize>) {
+    const START: &str = "AAA";
+    const END: &str = "ZZZ";
 
     let mut location = START;
     let mut steps = 0;
@@ -51,5 +58,52 @@ fn main() {
         }
     }
 
-    println!("Reached the end in {} steps", steps);
+    println!("Navigated from {} to {} in {} steps", START, END, steps);
+}
+
+fn part_two(map: &HashMap<&str, [&str; 2]>, directions: &Vec<usize>) {
+    let locations: Vec<&str> = map
+        .keys()
+        .filter_map(|&location| location.ends_with('A').then_some(location))
+        .collect();
+
+    let mut steps_to_destinations: Vec<u64> = vec![];
+
+    for &start in locations.iter() {
+        let mut location = start;
+        let mut steps: u64 = 0;
+
+        for &direction in directions.iter().cycle() {
+            steps += 1;
+            location = map.get(location).unwrap()[direction];
+            if location.ends_with('Z') {
+                steps_to_destinations.push(steps);
+                break;
+            }
+        }
+    }
+
+    if let Some(lcm) = lowest_common_multiple(&mut steps_to_destinations) {
+        println!("All the ghosts reached their destinations in {} steps", lcm);
+    }
+}
+
+fn lowest_common_multiple(numbers: &mut Vec<u64>) -> Option<u64> {
+    let lowest_number: u64 = *numbers.iter().min().unwrap();
+    let mut prime_factors: HashSet<u64> = HashSet::new();
+    let mut primes = Sieve::new();
+
+    for prime in primes.iter() {
+        for i in 0..numbers.len() {
+            if numbers[i] % prime == 0 {
+                prime_factors.insert(prime);
+                numbers[i] = numbers[i] / prime;
+            }
+        }
+        if prime > lowest_number || numbers.iter().all(|&n| n == 1) {
+            break;
+        }
+    }
+
+    return Some(prime_factors.iter().product());
 }
